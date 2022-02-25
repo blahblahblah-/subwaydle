26af9c505282ebc47eb8ed22180c2c194bebb755
Sunny Ng <sng@squarespace.com>
2022-02-24 23:29:59 -0500

    Use rough estimate travel_distance_factor to generate solutions that are not too out of the way

--- scripts/possible_guesses_generator.rb
+++ scripts/possible_guesses_generator.rb
@@ -1,6 +1,7 @@
 require "csv"
 require "set"
 require "json"
+require "geokit"
 
 patterns = {
   "weekday" => ["1", "2", "3", "4", "5", "6", "7", "A1", "A2", "B", "C", "D", "E", "F", "G", "J", "L", "M", "N", "Q", "R", "W", "SI", "FS", "GS", "H"],
@@ -25,11 +26,13 @@ patterns.each do |p, routes|
   solutions = {}
   station_stops = {}
   routings = {}
+  latlng = {}
 
   stations_csv = File.read('data/common/Stations.csv')
   csv = CSV.parse(stations_csv, headers: true)
   csv.each do |row|
     station_stops[row['GTFS Stop ID']] = []
+    latlng[row['GTFS Stop ID']] = Geokit::LatLng.new(row['GTFS Latitude'],row['GTFS Longitude'])
   end
 
   routes.each do |r|
@@ -68,7 +71,7 @@ patterns.each do |p, routes|
                 next if next_station1 && subrouting2.include?(next_station1)
                 subrouting2.each_with_index do |s3, i2n|
                   next if i2n == 0
-                  break if path1.include?(s3) || [transfers[s3]].flatten.compact.any? { |s| path1.include?(s) }
+                  break if subrouting1.include?(s3) || [transfers[s3]].flatten.compact.any? { |s| path1.include?(s) }
 
                   path2 = subrouting2[0..i2n]
                   next_station2 = subrouting2[i2n + 1]
@@ -89,7 +92,24 @@ patterns.each do |p, routes|
                         next if next_station2 && subrouting3.include?(next_station2)
                         subrouting3.each_with_index do |s4, i3n|
                           next if i3n == 0
-                          break if path1.include?(s4) || path2.include?(s4) || [transfers[s4]].flatten.compact.any? { |s| path1.include?(s) } || [transfers[s4]].flatten.compact.any? { |s| path2.include?(s) }
+                          break if subrouting1.include?(s4) || subrouting2.include?(s4) || [transfers[s4]].flatten.compact.any? { |s| path1.include?(s) } || [transfers[s4]].flatten.compact.any? { |s| path2.include?(s) }
+
+                          path3 = subrouting3[0..i3n]
+
+                          route_exists_from_begin_to_end = false
+                          ([transfers[s1]].flatten.compact + [s1]).each do |ts1|
+                            ([transfers[s4]].flatten.compact + [s4]).each do |ts2|
+                              station_stops[ts2].each do |sr|
+                                if routings[sr].include?(ts1)
+                                  one_route_stops = (routings[sr].index(ts1) - routings[sr].index(ts2)).abs
+                                  currnet_route_stops = path1.size + path2.size + path3.size - 2
+                                  if one_route_stops < currnet_route_stops
+                                    route_exists_from_begin_to_end = true
+                                  end
+                                end
+                              end
+                            end
+                          end
 
                           combo = [r1, r2, r3].map do |x|
                             if x.start_with?("A")
@@ -98,8 +118,13 @@ patterns.each do |p, routes|
                               x
                             end
                           end
+
+                          as_the_crow_flies = latlng[s1].distance_to(latlng[s4])
+                          estimated_travel_distance = latlng[s1].distance_to(latlng[s2]) + latlng[s2].distance_to(latlng[t1]) + latlng[t1].distance_to(latlng[s3]) + latlng[s3].distance_to(latlng[t2]) + latlng[t2].distance_to(latlng[s4])
+                          travel_distance_factor = estimated_travel_distance / as_the_crow_flies
+
                           if !answers.include?(combo)
-                            # puts "#{s1} #{r1} #{s2}-#{t1} #{r2} #{s3}-#{t2} #{r3}"
+                            # puts "#{s1} #{r1} #{s2}-#{t1} #{r2} #{s3}-#{t2} #{r3} #{as_the_crow_flies} mi vs. #{estimated_travel_distance} mi (#{travel_distance_factor})"
                             answers << combo
                             solutions[combo] = [
                               {
@@ -109,6 +134,7 @@ patterns.each do |p, routes|
                                 second_transfer_arrival: s3,
                                 second_transfer_departure: t2,
                                 destination: s4,
+                                travel_distance_factor: route_exists_from_begin_to_end ? 100 : travel_distance_factor,
                               }
                             ]
                           else
@@ -119,6 +145,7 @@ patterns.each do |p, routes|
                               second_transfer_arrival: s3,
                               second_transfer_departure: t2,
                               destination: s4,
+                              travel_distance_factor: route_exists_from_begin_to_end ? 100 : travel_distance_factor,
                             }
                           end
                         end
@@ -136,15 +163,17 @@ patterns.each do |p, routes|
 
   puts "Writing to JSON file - #{answers.size} entries"
 
+  picked_solutions = solutions.map { |k, v|
+    [k.join("-"), v.sort_by { |s| s[:travel_distance_factor] }.slice(0, v.size / 3).shuffle.first]
+  }.to_h
+
+  bad_solutions = picked_solutions.select { |_, v| v[:travel_distance_factor] == 100}.map { |k, _| k}
+
   file = File.open("../src/data/#{p}/answers.json", "w")
-  file.puts JSON.pretty_generate(answers.to_a.shuffle)
+  file.puts JSON.pretty_generate((answers.to_a - bad_solutions).shuffle)
   file.close
 
   file = File.open("../src/data/#{p}/solutions.json", "w")
-  file.puts JSON.pretty_generate(
-    solutions.map { |k, v|
-      [k.join("-"), v.shuffle.first]
-    }.to_h
-  )
+  file.puts JSON.pretty_generate(picked_solutions)
   file.close
 end
