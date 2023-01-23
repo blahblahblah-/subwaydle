029a28eeb925fbf10e2294e35dbc57bdd7069bfb
Sunny Ng <sunny@sunny-ng.com>
2023-01-22 23:37:19 -0500

    Add countdown

---
+++ src/components/Countdown.jsx
@@ -0,0 +1,38 @@
+import { useEffect, useState } from 'react';
+import { Header } from 'semantic-ui-react';
+
+const formatNumber = (number) => {
+  const numStr = Math.floor(number).toString();
+  if (numStr.length === 1) {
+    return '0' + numStr;
+  }
+  return numStr;
+}
+
+const Countdown = () => {
+  const midnight = new Date();
+  midnight.setHours(24);
+  midnight.setMinutes(0);
+  midnight.setSeconds(0);
+  midnight.setMilliseconds(0);
+
+  const [countDown, setCountDown] = useState(
+    (midnight - Date.now()) / 1000
+  );
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCountDown((midnight - Date.now()) / 1000);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [midnight]);
+
+  return (
+    <Header as='h5'>
+      Next Subwaydle in { formatNumber(countDown/3600) }:{ formatNumber(countDown/60 % 60) }:{ formatNumber(countDown % 60) }
+    </Header>
+  );
+}
+
+export default Countdown;
--- src/components/SolutionModal.jsx
+++ src/components/SolutionModal.jsx
@@ -4,6 +4,7 @@ import { Modal, Header, Button, Icon } from 'semantic-ui-react';
 import Stats from './Stats';
 import TrainBullet from './TrainBullet';
 import MapFrame from './MapFrame';
+import Countdown from './Countdown';
 
 import { todaysTrip, todaysSolution } from '../utils/answerValidations';
 import { shareStatus } from '../utils/share';
@@ -71,6 +72,7 @@ const SolutionModal = (props) => {
           <TrainBullet id={trip[1]} size='small' /> from { stations[solution.first_transfer_departure].name } to { stations[solution.second_transfer_arrival].name }<br />
           <TrainBullet id={trip[2]} size='small' /> from { stations[solution.second_transfer_departure].name } to { stations[solution.destination].name }
           <Stats isDarkMode={isDarkMode} stats={stats} />
+          <Countdown />
           <Button positive icon labelPosition='right' onClick={handleShareClick} className='share-btn'>
             { isShareButtonShowCopied ? 'Copied' : 'Share' }
             <Icon name={isShareButtonShowCopied ? 'check' : 'share alternate'} />
